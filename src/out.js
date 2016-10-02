0923f6eca0f09da5e8291ecd3fbd4852287a40bf
hari <harikrishnan.a@infibeam.net>
2016-10-02 21:07:49 +0530

    change dir

--- src/out.js
+++ src/out.js
@@ -2,7 +2,7 @@ module.exports = function(fs) {
   return {
     write: function(file, data){
       //var filePath = path.join(__dirname, '../out', file);
-      var filePath = ['/mnt/data/toddler/src', '../out', file].join('/')
+      var filePath = ['/home/hari/toddler/out', file].join('/')
       fs.writeFile(filePath, JSON.stringify(data), function(err) { if(err) {
           return console.log(err);
         }
