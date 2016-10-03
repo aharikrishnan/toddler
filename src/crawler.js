e86fff37978d2d19029572085db617bf2ff691d6
harikrishnan <harikrishnan.a@infibeam.net>
2016-10-03 06:05:28 +0530

    stability fixes

--- src/crawler.js
+++ src/crawler.js
@@ -1,24 +1,24 @@
 var casper = require("casper").create({viewportSize:{width:1366, height:950}});
 var fs = require("fs");
 casper.userAgent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.97 Safari/537.11");
-casper.options.waitTimeout = 300000;
-casper.options.stepTimeout = 300000;
+casper.options.waitTimeout = 360000;
+casper.options.stepTimeout = 360000;
 casper.options.verbose = true;
 casper.options.logLevel ="debug";
-var getRandomInt = function(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + min);
-};
-var getRandomCategory = function(subCategories, depth) {
-  var depth = depth || 0;
-  if (depth > 10) {
-    return{};
+var getCategories = function(subCategories, length) {
+  var categories = [], n = subCategories.length, j=0;
+  for(var i=0;i<Math.min(n, length); ){
+    var node =subCategories[j];
+    j = j+1;
+    if (node && /^http/.test(node.h)) {
+      categories.push(node);
+        i= i+1;
+    }
+    else{
+      n = n-1;
+    }
   }
-  var node = subCategories[getRandomInt(0, subCategories.length)];
-  if (node && /^http/.test(node.h)) {
-    return node;
-  } else {
-    return getRandomCategory(subCategories, depth + 1);
-  }
+  return categories;
 };
 var searchResultsLoad = function() {
   return casper.wait(700).evaluate(function() {
@@ -64,22 +64,23 @@ var scrape = function(index, categories) {
           var total = this.evaluate(function(){
             return $("#cards-holder .card-title").length;
           })
-          var grid=[], row = [];
+          var grid=[];
           var categoryTitle = category.n;
           var products = this.evaluate(extractDetails);
           for (var i = 0;i < products.length;i++) {
+            var row = []
             row.push(categoryTitle);
             row.push(products[i]);
             grid.push(row);
           }
           fs.write("./out/table.json", JSON.stringify(grid, null, "\t"), "a");
           table.concat(grid);
-          casper.wait(600).then(function() {
+          casper.wait(300).then(function() {
             scrape(index + 1, categories);
           })
         }, function() {
           this.captureSelector("./out/" + category.n + ".timeout.png".replace(/[^\x00-\x7F]/g, "-"), "body");
-        }, 300000);
+        }, 360000);
         }
         else{
           this.captureSelector("./out/" + category.n + ".fail.png".replace(/[^\x00-\x7F]/g, "-"), "body");
@@ -91,11 +92,16 @@ var scrape = function(index, categories) {
   }
 };
 var init = function(forest) {
-  var randomCategories = [];
+  var randomCategories = [], perCategory = 3;
+  fs.write("./out/categories.json", "------------Init-------------\n", "a");
   for (var i = 0;i < forest.length;i++) {
     var tree = forest[i];
-    var leaf = getRandomCategory(tree.l);
-    randomCategories.push(leaf);
+    categories  = tree.l || [];
+    selectedCategories = getCategories(categories, perCategory);
+    randomCategories = randomCategories.concat(selectedCategories);
+    fs.write("./out/categories.json", JSON.stringify(tree.n, null, "\t") + "\n", "a");
+    fs.write("./out/categories.json", JSON.stringify(selectedCategories, null, "\t") + "\n", "a");
+    fs.write("./out/categories.json", "------------------------\n", "a");
   }
   console.log(JSON.stringify(randomCategories));
   console.log(randomCategories.length);
